014f05787a6369d96a2774166cd995a0928ad1d4
sudarshan233 <nsridharsudarshan@gmail.com>
2025-06-27 11:17:51 +0530

    Progress: 1. The frontend UI with basic components and functionality has been    created successfully. 2. The correct letters and incorrect letters have been identified    successfully by the app which provides the divColor. Next Steps: 1. Connect the front-end with the model in the backend. 2. Fine-tune to model to generate Paragraph. 3. Fine-tune to model to generate Feedback. 4. Fine-tune to model to generate Game Levels..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import InputArea from "./Components/InputArea.jsx";
 import {useEffect, useState} from "react";
 function App() {
     const [input, setInput] = useState([]);
-    const logKey = (event) => {
+    const logChange = (event) => {
         const value = event.target.value.replace(/\n/g, '');
         setInput(value);
+    }
+    const logKey = (event) => {
+        console.log(event.key);
     }
     return(
       <div className="flex flex-col justify-center items-center h-screen">
           <TypingArea typed={input}/>
-          <InputArea handleChange={logKey}/>
+          <InputArea handleChange={logChange} handleKey={logKey}/>
       </div>
     )
 }
--- frontend/src/Components/InputArea.jsx
+++ frontend/src/Components/InputArea.jsx
@@ -1,7 +1,7 @@
 
 const InputArea = (props) => {
     return (
-        <textarea onChange={props.handleChange} className="h-72 w-5/6 border-2 border-black outline-0 rounded-xl p-4 text-3xl">
+        <textarea onChange={props.handleChange} onKeyDown={props.handleKey} className="h-72 w-5/6 border-2 border-black outline-0 rounded-xl p-4 text-3xl">
         </textarea>
     )
 }
